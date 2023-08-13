2c77167a73306eec80946f163663e23cd2bf6231
Vyash Bhawan <vyashbhawan@gmail.com>
2023-08-13 16:53:43 +0200

    added auth connection w backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/authenticate" Component={LoginPage} />
+      <Route path="/" Component={HomePage} />
     </Routes>
   );
 }
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
+import axios from '../api/axios';
+import { useNavigate } from "react-router-dom";
 
 export default function AuthPage() {
-  const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleFormType = () => {
     setIsLogin(!isLogin);
+  }
+
+  const handleLogin =  async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('/login', {email, password});
+      setEmail("");
+      setPassword("");
+      navigate("/");
+
+    } catch (error) {
+      console.log(error);
+    }
+    
   }
 
     return (
@@ -22,7 +42,7 @@ export default function AuthPage() {
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6"  onSubmit={handleLogin}>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                   Email address
@@ -32,6 +52,8 @@ export default function AuthPage() {
                     id="email"
                     name="email"
                     type="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                     autoComplete="email"
                     required
                     className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
@@ -55,6 +77,8 @@ export default function AuthPage() {
                     id="password"
                     name="password"
                     type="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                     autoComplete="current-password"
                     required
                     className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
@@ -67,7 +91,7 @@ export default function AuthPage() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-green-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                 >
-                  Sign in
+                  {isLogin ? 'Sign in' : 'Sign up'}
                 </button>
               </div>
             </form>
